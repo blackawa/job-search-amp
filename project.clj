b0a76abac9b09ea6c422bbbf963c998fb6c72b9b
blackawa <3104kurokawa@gmail.com>
2017-10-24 18:42:32 +0900

    :recycle: clojurify json string

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.9.0-beta2"]
+                 [org.clojure/data.json "0.2.6"]
                  [duct/core "0.6.1"]
                  [duct/module.logging "0.3.1"]
                  [duct/module.web "0.6.2"]
--- src/job_search_amp/view/layout.clj
+++ src/job_search_amp/view/layout.clj
@@ -1,8 +1,19 @@
 (ns job-search-amp.view.layout
-  (:require [garden.core :as garden]
+  (:require [clojure.data.json :as json]
+            [garden.core :as garden]
             [hiccup.core :as hiccup]))
 
-(defn- style []
+(def ^:private amp-boilerplate-style
+  "body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}")
+
+(def ^:private schema-json
+  (json/write-str {"@context" "http://schema.org"
+                   "@type" "NewsArticle"
+                   "headeline" "Open-source framework for publishing content"
+                   "datePublished" "2015-10-07T12:02:41Z"
+                   "image" ["logo.jpg"]}))
+
+(def ^:private custom-style
   (garden/css [:body {:background-color "#f1f1f1"}]))
 
 (defn html-amp [& body-content]
@@ -16,22 +27,12 @@
      [:title "Hello, AMP"]
      [:link {:rel "canonical" :href "http://example.ampproject.org/article-metadata.html"}]
      [:meta {:name "viewport" :content "width=device-width,minimum-scale=1,initial-scale=1"}]
-     [:script {:type "application/ld+json"}
-      "{
-        \"@context\": \"http://schema.org\",
-        \"@type\": \"NewsArticle\",
-        \"headline\": \"Open-source framework for publishing content\",
-        \"datePublished\": \"2015-10-07T12:02:41Z\",
-        \"image\": [
-          \"logo.jpg\"
-        ]
-      }"]
-     [:style {:amp-boilerplate ""}
-      "body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}"]
+     [:script {:type "application/ld+json"} schema-json]
+     [:style {:amp-boilerplate ""} amp-boilerplate-style]
      [:noscript
       [:style {:amp-boilerplate ""}
        "body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}"]]
      [:script {:async "" :src "https://cdn.ampproject.org/v0.js"}]
-     [:style {:amp-custom ""} (style)]]
+     [:style {:amp-custom ""} custom-style]]
     [:body
      body-content]]))
